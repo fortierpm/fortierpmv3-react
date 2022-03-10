c3023fb4aa0d89f41c78fb9876841a234c93eb22
Peter Fortier <fortier.pm@gmail.com>
2022-03-09 22:07:39 -0600

    Attempted Safari Fixes

--- src/components/CarouselSlide.js
+++ src/components/CarouselSlide.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 //import image from "../img/home-img/thumb/CosmosSeries002WaveThumb.jpg"
@@ -6,13 +5,15 @@ import { Link } from "react-router-dom";
 const CarouselSlide = ({ page, seriesPath, slideID, thumbPaths, series }) => {
     return (
         <div className="carousel-slide">
-            <div className="slide-grid" id={slideID}>
-                {thumbPaths[seriesPath].map((img, i) => {
-                    return (
-                        <img src={img} alt="Thumbnail for example piece" key={i}/>
-                    )
-                })}
-            </div>
+            {/* <div className="safari-backup-grid"> FAILED */}
+                <div className="slide-grid" id={slideID}>
+                    {thumbPaths[seriesPath].map((img, i) => {
+                        return (
+                            <img src={img} alt="Thumbnail for example piece" key={i}/>
+                        )
+                    })}
+                </div>
+            {/* </div> */}
             <h4 className="slide-title"><Link className="slide-link" to={`/${page}/${seriesPath}`}>{series}</Link></h4>
         </div>
     )
